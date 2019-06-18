0bfc2ce2ee701ee2abeb3a9b3808a5e522e9973b
Thomas <tnguyen.thomas@gmail.com>
2019-06-18 12:29:42 -0400

    added code to file.

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -12,11 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgimg: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hey Khoa")
+        var isThisSafe = true
+    
     }
-
+    
+    func rememberEverything() {
+        let neuron = 100
+        let memory = false
+        let brain = 1
+    }
+    
     @IBAction func unwindFromSkillVC(undwindSegue: UIStoryboardSegue) {
         
     }
