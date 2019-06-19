9072478be6d175407d90b16f1a2a09fed7ae5844
Thomas <tnguyen.thomas@gmail.com>
2019-06-19 09:54:59 -0400

    passing data through VC

--- app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/Controller/SkillVC.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class SkillVC: UIViewController {
 
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(player.desiredLeague)
 
         // Do any additional setup after loading the view.
     }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Thomas K Nguyen on 6/19/19.
+//  Copyright © 2019 tkTech. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var  desiredLeague: String!
+    var selectedSkillLevel: String!
+
+}
